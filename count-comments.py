48832eee2ef1926c01782179b017b6ebbf3381f8
Heather Michaud <hmm34@zips.uakron.edu>
2014-11-27 10:46:53 -0500

    Fix tabs

--- count-comments.py
+++ count-comments.py
@@ -7,12 +7,12 @@ from lxml import etree
 string = ""
 data = sys.stdin.readlines()
 for i in range(0, len(data)):
-	string += data[i]
+    string += data[i]
 
 tree = etree.fromstring(string)
 
 r = tree.xpath('//src:comment',
-	namespaces={'src': 'http://www.sdml.info/srcML/src',
-			    'cpp': 'http://www.sdml.info/srcML/cpp'})
+    namespaces={'src': 'http://www.sdml.info/srcML/src',
+                'cpp': 'http://www.sdml.info/srcML/cpp'})
 
 print(len(r))
