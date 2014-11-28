aa52cbc2a73398486ddb6c98483090ade35d34d1
Heather Michaud <hmm34@zips.uakron.edu>
2014-11-28 10:16:58 -0500

    Add test for comment count

---
+++ test/test.py
@@ -0,0 +1,11 @@
+import subprocess
+
+input = ["cat", "input.xml"]
+prog = ["python", "../count-comments.py"]
+
+pin = subprocess.Popen(input, stdout=subprocess.PIPE)
+pout = subprocess.Popen(prog, stdin=pin.stdout, stdout=subprocess.PIPE)
+
+out, err = pout.communicate()
+
+assert (out.rstrip('\n') == "1")
