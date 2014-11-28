1be2f3a998a3ae7c12516c127b2e10a7992f880a
Heather Michaud <hmm34@zips.uakron.edu>
2014-11-27 21:47:17 -0500

    Print function names

--- long-parameter-list.py
+++ long-parameter-list.py
@@ -1,19 +1,21 @@
 # Input: Source code in XML
-# Output: Functions matching a parameter list > n
+# Output: Function names matching a parameter list > n
 
 import util
 from lxml import etree
 
 tree = util.inputtree()
 
-n = 2
-query = '//src:function[count(src:parameter_list/src:param) > ' + `n` + ' ]'
-query +=' | '
-query += '//src:constructor[count(src:parameter_list/src:param) > ' + `n` + ' ]'
+n = 1
+predicate = '[count(src:parameter_list/src:param) > ' + `n` + ' ]'
+query = '//src:function' + predicate + ' | //src:constructor' + predicate
 
 r = tree.xpath(query,
     namespaces={'src': 'http://www.sdml.info/srcML/src',
                 'cpp': 'http://www.sdml.info/srcML/cpp'})
 
 for node in r:
-    print(etree.tostring(node))
+    s = node.xpath('(./src:name/src:name/text())[last()]',
+        namespaces={'src': 'http://www.sdml.info/srcML/src',
+                'cpp': 'http://www.sdml.info/srcML/cpp'})
+    print(s[0])
