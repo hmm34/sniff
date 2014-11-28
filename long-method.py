f63888a0a0d3970c5d1711033834da5d9664437a
Heather Michaud <hmm34@zips.uakron.edu>
2014-11-27 23:51:23 -0500

    Add long method > n lines

---
+++ long-method.py
@@ -0,0 +1,22 @@
+# Input: Source code in XML
+# Output: Function names whose contents exceed n lines
+
+import util
+from lxml import etree
+
+tree = util.inputtree()
+
+n = 200
+query = '//src:function | //src:constructor'
+
+r = tree.xpath(query,
+    namespaces={'src': 'http://www.sdml.info/srcML/src',
+                'cpp': 'http://www.sdml.info/srcML/cpp'})
+
+for node in r:
+	string = etree.tostring(node)
+	if string.count('\n') > n:
+		s = node.xpath('(./src:name/src:name/text())[last()]',
+            namespaces={'src': 'http://www.sdml.info/srcML/src',
+                        'cpp': 'http://www.sdml.info/srcML/cpp'})
+		print(s[0])
