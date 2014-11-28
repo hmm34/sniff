aa38767174860fb017d06e98c03d4f56317bb259
Heather Michaud <hmm34@zips.uakron.edu>
2014-11-28 14:14:52 -0500

    Move assertions into launch_test

--- test/test.py
+++ test/test.py
@@ -1,30 +1,31 @@
 import subprocess
 
-def launch_test(program):
+def launch_test(program, expected_output):
 	input = ["cat", "input.xml"]
 	pin = subprocess.Popen(input, stdout=subprocess.PIPE)
 	pout = subprocess.Popen(program, stdin=pin.stdout, stdout=subprocess.PIPE)
-	return pout.communicate()
+	out, err = pout.communicate()
+	assert(out.rstrip('\n') == expected_output)
 
 
 input = ["cat", "input.xml"]
 
 # test count-comments
 prog = ["python", "../count-comments.py"]
-out, err = launch_test(prog)
-assert (out.rstrip('\n') == "7")
+expect = "7"
+launch_test(prog, expect)
 
 # test class with 5 or more member variables
 prog = ["python", "../long-class.py", "5"]
-out, err = launch_test(prog)
-assert(out.rstrip('\n') == "../../testdata/project/circle.h: Circle")
+expect = "../../testdata/project/circle.h: Circle"
+launch_test(prog, expect)
 
 # test method with 5 or more lines
 prog = ["python", "../long-method.py", "5"]
-out, err = launch_test(prog)
-assert(out.rstrip('\n') == "../../testdata/project/circle.cpp: Circle")
+expect = "../../testdata/project/circle.cpp: Circle"
+launch_test(prog, expect)
 
 # test method with 3 or more parameters
 prog = ["python", "../long-parameter-list.py", "3"]
-out, err = launch_test(prog)
-assert(out.rstrip('\n') == "../../testdata/project/circle.cpp: reallyLongParameters")
+expect = "../../testdata/project/circle.cpp: reallyLongParameters"
+launch_test(prog, expect)
