05f394a79842cd560b1f2cf8105afe28defd308a
Heather Michaud <hmm34@zips.uakron.edu>
2014-12-11 11:13:53 -0500

    Use primitive predicate in parameter count

--- primitive-parameters.py
+++ primitive-parameters.py
@@ -11,15 +11,32 @@ n = parser.parse_args().n
 tree = util.inputtree()
 
 predicate = '[count(src:parameter_list/src:parameter) > ' + `n` + ' ]'
+
+types = util.primitives()
+types_query = "[./src:decl/src:type/src:name[ "
+for i in range(0, len(types)):
+    types_query += ". = '" + types[i] + "' "
+    if i != len(types) - 1:
+        types_query += " or "
+types_query += " ]]"
+
+predicate = "[count(src:parameter_list/src:parameter" + types_query + ") > " + `n` + ' ]'
 query = '//src:function_decl' + predicate + ' | //src:constructor_decl' + predicate
 
 r = tree.xpath(query,
     namespaces=util.srcml_ns())
 
 for node in r:
+    #print("===========")
+    #print(etree.tostring(node))
     info = util.filename(node)
 
     s = node.xpath('./src:name/text()',
         namespaces=util.srcml_ns())
+
+    if (len(s) <= 0):
+        s = node.xpath('./src:name/src:name/text()',
+        namespaces=util.srcml_ns())
+
     info += ": " + s[0]
     print(info)
